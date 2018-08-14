907eb0c470903a30ec6380787f80b6a1b32bee7a
youngil2473-cmis <youngil2473@cmis.ac.th>
2018-08-14 09:31:56 +0700

    Started CS Fundamentals I

---
+++ FundamentalOne/FundamentalOne.java
@@ -0,0 +1,27 @@
+public class FundamentalOne
+{
+    public static void main (String args [])
+    {
+    } // end method main
+    
+    public static void dataTypes ()
+    {
+        byte B = 127 ;
+        short S = 32767 ;
+        int I = 237462848 ;
+        long L = 3787182587L ;
+        float F = 3.14f ;
+        double D = 453.5454D ;
+        boolean b = false ;
+        char C = 'C' ;
+        String ExpByte = "Byte is between -128 and 127. \n We can use them to represent student class numbers, numbers of cookies in a jar, and the number of fingers on a hand" ;
+        String ExpShort = "Short is between -32768 and 32767. \n We can use them to represent the radius of Earth(KM), distance from Earth to Moon(KM), and the amount of sleep I need per day(minutes)" ; 
+        String ExpInt = "Integer is between -2^31 and 2^31-1. \n We can use them to represet the number of Earthlings, about the amount of time I have left on Earth in seconds (68 years), and the number of cells in my toenail";
+        String ExpLong = "Long is represent BIG BOI numbers. I quit to define its range. \n We can use them to represent the number of cells in our body, the number of days required to travel to Andromedea Galaxy, and the number of hours ";
+        String ExpFloat ;
+        String ExpDouble ;
+        String ExpBoolean ;
+        String ExpChar ;
+        // Explanation for each Data Type 
+    } // end method dataTypes
+} // end class FundamentalOne
