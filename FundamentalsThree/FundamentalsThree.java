472d46a139206587875b5e303152cd6a2da5e01f
d@g <d@g>
2018-10-26 09:53:38 +0700

    begn 9

--- FundamentalsThree/FundamentalsThree.java
+++ FundamentalsThree/FundamentalsThree.java
@@ -38,5 +38,92 @@ public class FundamentalsThree
     {
         arr [y][x] = v;
     }
-}
 
+    public static void print2DArray(int[][] array, boolean rowMajor)
+    {
+        if (rowMajor == true)
+        {
+            for (int y = 0; y < array.length; y ++)
+            {     
+                for (int x = 0; x < array[y].length; x ++)
+                {
+                    System.out.print(array[y][x] + " ");
+                }
+                System.out.println("");
+            }
+        }
+        else
+        {
+            for (int y = 0; y < array.length; y ++)
+            {     
+                for (int x = 0; x < array[y].length; x ++)
+                {
+                    System.out.print(array[x][y] + " ");
+                }
+                System.out.println("");
+            }
+        }
+    } // end of 5
+
+    public static void snakePrint(int[][] arr)
+    {
+        for (int y = 0; y < arr.length; y ++)
+        {
+            if (y % 2 == 0)
+            {
+                for (int x = 0; x < arr[y].length; x ++)
+                {
+                    System.out.print(arr[y][x] + " ");
+                }
+            }
+            else
+            {
+                for (int x = arr[y].length - 1; x >= 0 ; x --)
+                {
+                    System.out.print(arr[y][x] + " ");
+                }
+            }
+        }
+        System.out.println("");
+    } // end of 6
+
+    public static String[][] locate(String[][] arr)
+    {
+        int randInt = (int)(Math.random() * 9);
+        int randX = (int)(Math.random() * arr.length);
+        int randY = (int)(Math.random() * arr[0].length);
+        String [][] result = new String [arr.length][arr[0].length];
+        for (int y = 0; y < arr.length; y ++)
+        {
+            for (int x = 0; x < arr[y].length ; x ++)
+            {
+                if (x == randX && y == randY)
+                    result [x][y] = String.format("%d",randInt);
+                else
+                    result [x][y] = "#";
+            }
+        }
+        return result;
+    } // end of 7
+    
+    public static int[][] replace(int[][] array, int threshold, int newValue)
+    {
+        int [][] result = new int [array.length][array[0].length];
+        for (int y = 0; y < array.length; y ++)
+        {
+            for (int x = 0; x < array[y].length; x ++)
+            {
+                if (array [y][x] <= threshold)
+                result [y][x] = array [y][x];
+                else 
+                result [y][x] = newValue;
+            }
+        }
+        return result;
+    } // end of 8
+    
+    public static double[][] shift(double[][] arr, int row)
+    {
+        return null;
+    }
+}
