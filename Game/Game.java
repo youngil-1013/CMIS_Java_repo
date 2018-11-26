4a9f759b988224db13d84945245af331a6a68136
d <d>
2018-11-26 11:54:57 +0700

    String Anna = Cancer;

--- Game/Game.java
+++ Game/Game.java
@@ -3,8 +3,23 @@ public class Game
 {
     public static void main (String [] args)
     {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("What is the map's width");
+        String Swidth = scan.next();
+        System.out.println("What is the map's height");
+        String Sheight = scan.next();
+        int width = Integer.parseInt(Swidth);
+        int height = Integer.parseInt(Sheight);
+        // Input for Map Size
         boolean gameOver = false;
-        String [][] Map = {{"o","o","o"},{"o","o","o"},{"o","o","o"}};
+        String [][] Map = new String [height][width];
+        for (int y = 0; y < height; y ++)
+        {     
+            for (int x = 0; x < width; x ++)
+            {
+                Map [y][x] = "o";
+            }
+        }
         int exitY = (int) (Math.random() * (Map.length));
         int exitX = (int) (Math.random() * (Map[0].length));
         Map [exitY][exitX] = "X";
@@ -16,19 +31,72 @@ public class Game
             playerX = (int) (Math.random() * (Map.length));
         }
         Map [playerY][playerX] = "P";
+        int Barrier = 3;
+        for (int y = 0; y < height; y ++)
+        {     
+            for (int x = 0; x < width; x ++)
+            {
+                Barrier = (int) (Math.random() * 4);
+                if (Barrier == 0 && Map [y][x] == "o")
+                {
+                    Map [y][x] = Character.toString((char)144);
+                }
+            }
+        }
         Print (Map);
         // Made Map
-        Scanner scan = new Scanner (System.in);
-
         while (gameOver == false)
         {
             String input = scan.next();
-            if (input == "1")
+            if (input.equals("w"))
             {
-                Map [playerY - 1][playerX] = Map [playerY][playerX];
-                Map [playerY][playerX] = "o";
+                if (Map [playerY - 1][playerX] != "|")
+                {
+                    Map [playerY - 1][playerX] = Map [playerY][playerX];
+                    Map [playerY][playerX] = "o";
+                    playerY = playerY - 1; 
+                    playerX = playerX; 
+                }
                 Print (Map);
-                gameOver=true;
+            }
+            else if (input.equals("s"))
+            {
+                if (Map [playerY + 1][playerX] != "|")
+                {
+                    Map [playerY + 1][playerX] = Map [playerY][playerX];
+                    Map [playerY][playerX] = "o";
+                    playerY = playerY + 1; 
+                    playerX = playerX;
+                }
+                Print (Map);
+            }
+            else if (input.equals("a"))
+            {
+                if (Map [playerY][playerX - 1] != "|")
+                {
+                    Map [playerY][playerX - 1] = Map [playerY][playerX];
+                    Map [playerY][playerX] = "o";
+                    playerY = playerY; 
+                    playerX = playerX - 1;
+                }
+                Print (Map);
+            }
+            else if (input.equals("d"))
+            {
+                if (Map [playerY][playerX + 1] != "|")
+                {
+                    Map [playerY][playerX + 1] = Map [playerY][playerX];
+                    Map [playerY][playerX] = "o";
+                    playerY = playerY; 
+                    playerX = playerX + 1;
+                }
+                Print (Map);
+            }
+
+            if (playerY == exitY && playerX == exitX)
+            {
+                gameOver = true; 
+                System.out.println("U w U\nYOU WIN BOI");
             }
         }
     }
