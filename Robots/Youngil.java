66e2af4ea5325eb68430f8e70df13012624ddb46
d@d <d@d>
2019-01-25 11:00:25 +0700

    robot is dumb

--- Robots/Youngil.java
+++ Robots/Youngil.java
@@ -22,12 +22,13 @@ public class Youngil extends Robot
      * isClearDown() => true means no block down
      * setData(idx, int)
      * getData(idx)
-     * 0: State (0 = Corner, 1 = foundCorner/nullWidth, 2 = foundWidth/nullHeight)
+     * 0: State (0 = Corner, 1 = foundCorner/nullWidth, 2 = foundWidth/nullHeight,
+     * 3 = move left/right, 4 = move up, 5 = found corner of Box)
      * 1: width;
      * 2: height;
-     * 3
-     * 4
-     * 5
+     * 3: currentWidth;
+     * 4: currentHeight;
+     * 5: right/left (0/1)
      * 6
      * 7
      * 8
@@ -74,10 +75,56 @@ public class Youngil extends Robot
                 setData(2, getData(2)+1);
             }
         }
-        else
+        else if (getData(0) == 3)
         {
-            //System.out.println(getData(1));
-            System.out.println(getData(2));
+            if (getData (5) == 0 && getData(3) < getData(1))//direction we want is right(left edge rn)
+            {
+                right();
+                setData(3, getData(3) + 1);//adds 1 to currentWidth
+            }
+            else if (getData(5) == 1 && getData(3) < getData(1))//direction we want is left(right edge rn)
+            {
+                left();
+                setData(3, getData(3) + 1);//adds 1 to currentWidth
+            }
+            else if ((getData(5) == 0 && !isClearRight()))//direction we are going is right
+            {
+                if (getData(3) != getData(1))
+                {
+                    setData(0, 5);
+                }
+                else
+                {
+                    setData(5, 1);//revert the direction we want to left
+                    setData(3, 0);//reset currentWidth;
+                    setData(0, 4);//move up 1
+                }
+            }
+            else if ((getData(5) == 1 && !isClearLeft()))//direction we are going is left
+            {
+                if (getData(3) != getData(1))
+                {
+                    setData(0, 5);
+                }
+                else if (getData(3) == getData(1))
+                { 
+                    setData(5, 0);//revert the direction we want to right
+                    setData(3, 0);//reset currentWidth
+                    setData(0, 4);//move up 1
+                }
+            }
         }
+        else if (getData(0) == 4)
+        {
+            up();
+            setData(0, 3);
+        }
+        else if (getData(0) == 5)
+        {
+            System.out.print("fuond");
+            setData(0,66);
+        }
+        
     }
 }
+
